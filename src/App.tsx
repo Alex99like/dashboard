a2078d097f425bca0c345ed60ad2f47014cb6db5
Alexander <aleksievisa@gmail.com>
2023-05-13 22:29:59 +0300

    feat: add component navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Box, CssBaseline, ThemeProvider } from '@mui/material'
 import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Navbar } from '@/scenes/navbar'
+
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -12,6 +14,19 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
+          <Box width={'100%'} height={'100%'} padding={'1rem 2rem 4rem 2rem'}>
+            <Navbar />
+            <Routes>
+              <Route 
+                path='/' 
+                element={<div>Dashboard Page</div>} 
+              />
+              <Route 
+                path='/predictions' 
+                element={<div>Predictions Page</div>} 
+              />
+            </Routes>
+          </Box>
         </ThemeProvider>
       </BrowserRouter>
     </div>
