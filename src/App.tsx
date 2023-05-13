6f545510d59296f60bb201b8c536ce14fc648d47
Alexander <aleksievisa@gmail.com>
2023-05-13 21:30:20 +0300

    feat: add configuration style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { BrowserRouter } from 'react-router-dom'
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
   return (
     <div className={'app'}>
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
